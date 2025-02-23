6d03d4008b7cb53099d0cbffc1082646bd7be246
Sarthak Neupane <sarthakneup11@gmail.com>
2025-02-23 11:05:51 +0545

    Update

--- assignment6.py
+++ assignment6.py
@@ -28,5 +28,5 @@ while True:
     except:
          print("Invalid input")
     continue
-print("Maximum is", largest)
-print("Minimum is", smallest)
+print("Maximum is:", largest)
+print("Minimum is:", smallest)
